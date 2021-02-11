6db233d2b4144fe3ed4fdd47b048f51b032ca881
Nikhil-997 <plavazhin@gmail.com>
2021-02-11 09:53:20 +0530

    admin

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -11,9 +11,10 @@
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
+            <li><a href="">Users Online: <?php  echo users_online();  ?></a></li>   
             <li><a href="../index.php">HOME SITE</a></li>
                 
-  
+              
                 
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>
